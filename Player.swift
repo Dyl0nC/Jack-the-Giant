38f0712db70a30762d18c2727224f6ed7b36b7d3
Dylan Cunningham <DylanC@Dylans-MacBook-Pro.local>
2017-06-18 17:23:51 -0600

    Jack Moving

---
+++ Player.swift
@@ -0,0 +1,54 @@
+//
+//  Player.swift
+//  JackTheGiant
+//
+//  Created by Dylan Cunningham on 6/18/17.
+//  Copyright © 2017 Dylan Cunningham. All rights reserved.
+//
+
+import SpriteKit
+
+class Player: SKSpriteNode {
+    
+    private var textureAtlas = SKTextureAtlas()
+    private var playerAnimation = [SKTexture]()
+    private var animatePlayerAction = SKAction()
+    
+    func initPlayerAndAnimations() {
+        textureAtlas = SKTextureAtlas(named: "Player.atlas")
+        
+        for i in 2...textureAtlas.textureNames.count {
+            let name = "Player \(i)"
+            playerAnimation.append(SKTexture(imageNamed: name))
+        }
+        
+        animatePlayerAction = SKAction.animate(with: playerAnimation, timePerFrame: 0.08, resize: true, restore: false)
+    }
+    
+    func movePlayer(moveLeft: Bool) {
+        
+        if moveLeft {
+            self.position.x = self.position.x - 12
+        } else {
+            self.position.x = self.position.x + 12
+        }
+        
+    }
+    
+    func animatePlayer(moveLeft: Bool) {
+        
+        if moveLeft {
+            self.xScale = -fabs(self.xScale)
+        } else {
+            self.xScale = fabs(self.xScale)
+        }
+        
+        self.run(SKAction.repeatForever(animatePlayerAction), withKey: "Animate")
+    }
+    
+    func stopPlayerAnimation() {
+        self.removeAction(forKey: "Animate")
+        self.texture = SKTexture(imageNamed: "Player 1")
+        self.size = (self.texture?.size())!
+    }
+}
